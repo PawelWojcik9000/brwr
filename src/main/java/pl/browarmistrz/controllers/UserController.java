3199d36e6adbaef54c2560a106740d6d403926eb
PawelWojcik9000 <pavelec@gmail.com>
2018-01-25 19:16:48 +0100

    push 25.01.2018

--- src/main/java/pl/browarmistrz/controllers/UserController.java
+++ src/main/java/pl/browarmistrz/controllers/UserController.java
@@ -1,5 +1,43 @@
 package pl.browarmistrz.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.browarmistrz.entities.User;
+import pl.browarmistrz.repositories.UserRepository;
+
+@Controller
+@RequestMapping("/user")
 public class UserController {
+	
+	private final UserRepository userRepository;
+	
+	@Autowired
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@GetMapping("/adduser")
+	public String addUser(Model model) {
+		model.addAttribute("adduser", new User());
+		return "adduserform";
+	}
+	@PostMapping("/adduser")
+	public String processAddUser(@Valid User user, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "adduserform";
+		} else {
+			userRepository.save(user);
+			return "redirect:/recipe/publicrecipes";
+		}
+	}
+	
 
 }
